d4dafac339bbb6befe848a12020c36e97bf58338
DiegoPiza <93541319+DiegoPiza@users.noreply.github.com>
2024-01-22 18:16:58 -0500

    Update TracVelocity.m

--- Low_level_functions/TracVelocity.m
+++ Low_level_functions/TracVelocity.m
@@ -35,7 +35,7 @@ function [eucl, dx, dy, dz] = TracVelocity(TrackingData, f, gaus_kernel)
         inv = isnan(eucl);
         eucl = fillmissing(eucl, 'linear', 'EndValues', 'none');
         sr = 1 / f; % Converting sampling rate to hertz
-        sigma = floor(0.2 * sr); % 200 ms window
+        sigma = floor(0.25 * sr); % 250 ms window
         L = sigma * 12; % Length of 6 sigma each side
         alpha = ((L - 1) / sigma) / 2;
         h = 1 / (sqrt(2 * pi) * sigma); % Height of Gaussian
@@ -43,24 +43,5 @@ function [eucl, dx, dy, dz] = TracVelocity(TrackingData, f, gaus_kernel)
         w(w < h * 1.e-3) = []; % Deleting Gaussian edges
         eucl = conv(eucl, w, 'same');
         eucl(inv) = nan;
-    elseif gaus_kernel == 2
-        inv = isnan(eucl);
-        dx = fillmissing(dx, 'linear', 'EndValues', 'none');
-        dy = fillmissing(dy, 'linear', 'EndValues', 'none');
-        dz = fillmissing(dz, 'linear', 'EndValues', 'none');
-        sr = 1 / f; % Converting sampling rate to hertz
-        sigma = floor(0.05 * sr); % 250 ms window
-        L = sigma * 12; % Length of 6 sigma each side
-        alpha = ((L - 1) / sigma) / 2;
-        h = 1 / (sqrt(2 * pi) * sigma); % Height of Gaussian
-        w = gausswin(L, alpha) * h; % The exact correspondence with the standard deviation of a Gaussian probability density function is σ = (L – 1) / (2α).
-        w(w < h * 1.e-3) = []; % Deleting Gaussian edges
-        dx = conv(dx, w, 'same');
-        dy = conv(dy, w, 'same');
-        dz = conv(dz, w, 'same');
-        eucl = sqrt((dx.^2) + (dy.^2) + (dz.^2)); % Pythagoras
-        eucl(inv) = nan;
     end
-    
-    % Additional processing steps could be added here
 end
